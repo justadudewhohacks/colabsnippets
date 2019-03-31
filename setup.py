4f8534c45ebe42d5ac398ff20b410073be9e4a02
vincent <muehler.v@gmail.com>
2019-03-31 19:08:25 +0200

    export nn properly

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
   name='colabsnippets',
-  version='0.0.2',
+  version='0.0.3',
   author='justadudewhohacks',
-  packages=['colabsnippets']
+  packages=['colabsnippets', 'colabsnippets.nn']
 )
