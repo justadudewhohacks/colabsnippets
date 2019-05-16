9bbc75c6d9d21c7f457cbd50ee08c318dff88da6
vincent <muehler.v@gmail.com>
2019-05-16 12:50:26 +0200

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
   name='colabsnippets',
-  version='0.0.14',
+  version='0.0.15',
   author='justadudewhohacks',
   packages=['colabsnippets', 'colabsnippets.nn', 'colabsnippets.age_recognition', 'colabsnippets.age_gender_recognition']
 )
