2456889d38a5c4fe5412308cb954b8702f4fec62
vincent <muehler.v@gmail.com>
2019-05-16 10:12:11 +0200

    DataDownloader - make downloading of shard data more flexible

--- colabsnippets/DataDownloader.py
+++ colabsnippets/DataDownloader.py
@@ -15,21 +15,16 @@ class DataDownloader():
     self.drive = GoogleDrive(gauth)
     self.data_dir = data_dir
 
-  def download_images_and_landmarks(self, db, images_id, landmarks_id = None):
+  def download_shard_data(self, db, shard, data_keys):
     data_dir = "./data/{}".format(db)
-    self.drive.CreateFile({ 'id': images_id }).GetContentFile("{}/images.7z".format(data_dir))
-
-    ts = time.time()
-    subprocess.run(["p7zip", "-d", "./images.7z"], cwd = data_dir)
-    print("unzipping images done in {}s".format(time.time() - ts))
-
-    if landmarks_id is not None:
-      self.drive.CreateFile({ 'id': landmarks_id }).GetContentFile("{}/landmarks.7z".format(data_dir))
+    for data_key in data_keys:
+      zip_file = "{}.7z".format(data_key)
+      self.drive.CreateFile({ 'id': shard[data_key] }).GetContentFile("{}/{}".format(data_dir, zip_file))
       ts = time.time()
-      subprocess.run(["p7zip", "-d", "./landmarks.7z"], cwd = data_dir)
-      print("unzipping landmarks done in {}s".format(time.time() - ts))
+      subprocess.run(["p7zip", "-d", "./{}".format(zip_file)], cwd = data_dir)
+      print("unzipping {} done in {}s".format(zip_file, time.time() - ts))
 
-  def download_data(self, dbs_dict):
+  def download_data(self, dbs_dict, additional_data_keys = []):
     mk_dir_if_not_exists(self.data_dir)
 
     ts = time.time()
@@ -40,6 +35,6 @@ class DataDownloader():
 
       for shard in dbs_dict[db]:
         print("downloading data for shard {}".format(shard['shard_id'] if 'shard_id' in shard else '0'))
-        self.download_images_and_landmarks(db, shard['images'], shard['landmarks'] if 'landmarks' in shard else None)
+        self.download_shard_data(db, shard, ['images'] + additional_data_keys)
 
     print("download_data - total time: {}s".format(time.time() - ts))
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
   name='colabsnippets',
-  version='0.0.13',
+  version='0.0.14',
   author='justadudewhohacks',
   packages=['colabsnippets', 'colabsnippets.nn', 'colabsnippets.age_recognition', 'colabsnippets.age_gender_recognition']
 )
