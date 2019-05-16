b373e6964e88f433a6c0a7229eaac1fa25a5d981
vincent <muehler.v@gmail.com>
2019-05-16 09:31:38 +0200

    allow DataDownloader to download shards without landmark files

--- colabsnippets/DataDownloader.py
+++ colabsnippets/DataDownloader.py
@@ -15,18 +15,19 @@ class DataDownloader():
     self.drive = GoogleDrive(gauth)
     self.data_dir = data_dir
 
-  def download_images_and_landmarks(self, db, images_id, landmarks_id):
+  def download_images_and_landmarks(self, db, images_id, landmarks_id = None):
     data_dir = "./data/{}".format(db)
     self.drive.CreateFile({ 'id': images_id }).GetContentFile("{}/images.7z".format(data_dir))
-    self.drive.CreateFile({ 'id': landmarks_id }).GetContentFile("{}/landmarks.7z".format(data_dir))
 
     ts = time.time()
     subprocess.run(["p7zip", "-d", "./images.7z"], cwd = data_dir)
     print("unzipping images done in {}s".format(time.time() - ts))
 
-    ts = time.time()
-    subprocess.run(["p7zip", "-d", "./landmarks.7z"], cwd = data_dir)
-    print("unzipping landmarks done in {}s".format(time.time() - ts))
+    if landmarks_id is not None:
+      self.drive.CreateFile({ 'id': landmarks_id }).GetContentFile("{}/landmarks.7z".format(data_dir))
+      ts = time.time()
+      subprocess.run(["p7zip", "-d", "./landmarks.7z"], cwd = data_dir)
+      print("unzipping landmarks done in {}s".format(time.time() - ts))
 
   def download_data(self, dbs_dict):
     mk_dir_if_not_exists(self.data_dir)
@@ -39,6 +40,6 @@ class DataDownloader():
 
       for shard in dbs_dict[db]:
         print("downloading data for shard {}".format(shard['shard_id'] if 'shard_id' in shard else '0'))
-        self.download_images_and_landmarks(db, shard['images'], shard['landmarks'])
+        self.download_images_and_landmarks(db, shard['images'], shard['landmarks'] if 'landmarks' in shard else None)
 
     print("download_data - total time: {}s".format(time.time() - ts))
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
   name='colabsnippets',
-  version='0.0.12',
+  version='0.0.13',
   author='justadudewhohacks',
   packages=['colabsnippets', 'colabsnippets.nn', 'colabsnippets.age_recognition', 'colabsnippets.age_gender_recognition']
 )
