1e7a017632484e13b8343203be14ff3f144b2318
vincent <muehler.v@gmail.com>
2019-08-15 13:02:07 +0200

    adds fpn calculate_stats

--- colabsnippets/face_detection/fpn/__init__.py
+++ colabsnippets/face_detection/fpn/__init__.py
@@ -1,2 +1,3 @@
 from .FPNBase import FPNBase
 from .generate_anchors import create_anchor, generate_anchors
+from .calculate_stats import calculate_stats
---
+++ colabsnippets/face_detection/fpn/calculate_stats.py
@@ -0,0 +1,44 @@
+import numpy as np
+
+from ..calculate_iou import calculate_iou
+
+def calculate_stats(inputs, score_thresh = 0.5, iou_threshs = [0.5]):
+  gt_pos_by_stage = [np.where(inputs["gt_masks_by_stage"][s] != 0) for s in range(0, num_stages)]
+  num_gt_anchors = np.sum([gt_pos_by_stage[s][0].shape[0] for s in range(0, num_stages)])
+  num_tps_score = np.sum([np.where(inputs["batch_scores_by_stage"][s][gt_pos_by_stage[s]] > score_thresh)[0].shape[0] for s in range(0, num_stages)])
+
+  batch_size = len(inputs["batch_pred_boxes"])
+
+  num_gt_boxes = 0
+  num_preds = 0
+  num_tps = [0 for i in range (0, len(iou_threshs))]
+  batch_good_boxes = [[[] for b in range(0, batch_size)] for i in range (0, len(iou_threshs))]
+  for b in range(0, batch_size):
+    gt_boxes = inputs["batch_gt_boxes"][b]
+    pred_boxes = inputs["batch_pred_boxes"][b]
+    num_preds += len(pred_boxes)
+    num_gt_boxes += len(gt_boxes)
+    for gt_box in gt_boxes:
+      ious = [calculate_iou(np.array(gt_box), np.array(pred_box)) for pred_box in pred_boxes]
+
+      for iou_idx in range (0, len(iou_threshs)):
+        iou_thresh = iou_threshs[iou_idx]
+
+        for i, iou in enumerate(ious):
+          if iou > iou_thresh:
+            batch_good_boxes[iou_idx][b].append(pred_boxes[i])
+
+        for iou in ious:
+          if iou > iou_thresh:
+            num_tps[iou_idx] += 1
+            break
+
+  return {
+    "num_tps": num_tps,
+    "num_matches": [np.sum([len(good_boxes) for good_boxes in batch_good_boxes[i]]) for i in range (0, len(iou_threshs))],
+    "num_tps_score": num_tps_score,
+    "num_preds": num_preds,
+    "num_gt_boxes": num_gt_boxes,
+    "num_gt_anchors": num_gt_anchors,
+    "batch_good_boxes": batch_good_boxes
+  }
