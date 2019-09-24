401f8a4eda9f3c906d0de5edfb698455f2b01d8f
vincent <muehler.v@gmail.com>
2019-09-24 21:33:49 +0200

    ...

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
   name='colabsnippets',
-  version='0.0.32',
+  version='0.0.33',
   author='justadudewhohacks',
   packages=['colabsnippets', 'colabsnippets.nn', 'colabsnippets.age_recognition', 'colabsnippets.age_gender_recognition', 'colabsnippets.face_detection', 'colabsnippets.face_detection.fpn', 'colabsnippets.face_detection.object_detector']
 )
