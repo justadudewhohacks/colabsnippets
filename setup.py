2761ebf8316c2aa4a8caff1cb12ebaff88b94f0b
vincent <muehler.v@gmail.com>
2019-09-30 21:55:03 +0200

    ...

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from setuptools import setup
 
 setup(
   name='colabsnippets',
-  version='0.0.67',
+  version='0.0.68',
   author='justadudewhohacks',
   packages=['colabsnippets', 'colabsnippets.nn', 'colabsnippets.age_recognition', 'colabsnippets.age_gender_recognition', 'colabsnippets.face_detection', 'colabsnippets.face_detection.fpn', 'colabsnippets.face_detection.object_detector'],
-    install_requires=['tensorflow', 'numpy', 'cv2']
+    install_requires=['tensorflow', 'numpy', 'python-opencv']
 )
