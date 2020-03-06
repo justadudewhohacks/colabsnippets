925547a516685aba406e839f8fedc65a24cda474
vincent <muehler.v@gmail.com>
2020-03-06 17:00:24 +0100

    ...

---
+++ colabsnippets/face_detection/augment/resize_to_fixed.py
@@ -0,0 +1,15 @@
+from colabsnippets.utils import abs_bbox_coords, rel_bbox_coords, rel_landmarks_coords, abs_landmarks_coords
+from .resize_by_ratio import resize_by_ratio
+
+
+def resize_to_fixed(img, boxes, landmarks, image_size, is_relative_coords=False):
+  im_h, im_w = img.shape[0:2]
+  s = image_size / max(im_h, im_w)
+  if is_relative_coords:
+    boxes = [abs_bbox_coords(box, img.shape[0:2]) for box in boxes]
+    landmarks = [abs_landmarks_coords(l, img.shape[0:2]) for l in landmarks]
+  img, boxes, landmarks = resize_by_ratio(img, boxes, landmarks, s, s)
+  if is_relative_coords:
+    boxes = [rel_bbox_coords(box, img.shape[0:2]) for box in boxes]
+    landmarks = [rel_landmarks_coords(l, img.shape[0:2]) for l in landmarks]
+  return img, boxes, landmarks
