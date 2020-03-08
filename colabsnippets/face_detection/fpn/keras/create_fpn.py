4c3bf24c60a558b776c23fda0a2c3224381ef40c
vincent <muehler.v@gmail.com>
2020-03-08 11:25:56 +0100

    ...

--- colabsnippets/face_detection/fpn/keras/create_fpn.py
+++ colabsnippets/face_detection/fpn/keras/create_fpn.py
@@ -2,10 +2,45 @@ from keras.layers import Input
 
 import colabsnippets.face_detection.fpn.keras.layers as L
 
+def make_heads(stage_outputs, stage_anchors, simple_heads=True, is_sigmoid_offsets=False, num_class_outputs=1):
+  outputs = []
+  for stage_idx, stage_output in enumerate(stage_outputs):
+    num_anchors = len(stage_anchors[stage_idx])
+    for anchor_idx in range(0, num_anchors):
+      # backward compatibility
+      if num_anchors == 1:
+        anchor_id = 'stage_' + str(stage_idx)
+      else:
+        anchor_id = 's_' + str(stage_idx) + '_a_' + str(anchor_idx)
+
+      print(anchor_id)
+
+      if simple_heads:
+        offsets = L.conv1x1(stage_output, 2,
+                            'conv_offsets_' + anchor_id if is_sigmoid_offsets else 'output_' + anchor_id + '_offsets')
+        if is_sigmoid_offsets:
+          offsets = L.sigmoid(offsets, 'output_' + anchor_id + '_offsets')
+        scales = L.conv1x1(stage_output, 2, 'output_' + anchor_id + '_scales')
+        scores_logits = L.conv1x1(stage_output, 1, 'conv_scores_' + anchor_id)
+      else:
+        offsets = L.depthwise_separable_conv2d(stage_output, 2,
+                            'conv_offsets_' + anchor_id if is_sigmoid_offsets else 'output_' + anchor_id + '_offsets')
+        scales = L.depthwise_separable_conv2d(stage_output, 2, 'output_' + anchor_id + '_scales')
+        scores_logits = L.depthwise_separable_conv2d(stage_output, num_class_outputs, 'conv_scores_' + anchor_id)
+
+
+
+      scores = L.sigmoid(scores_logits, 'output_' + anchor_id + '_object')
+      scores2 = L.sigmoid(scores_logits, 'output_' + anchor_id + '_no_object')
+
+      outputs.append(offsets)
+      outputs.append(scales)
+      outputs.append(scores)
+      outputs.append(scores2)
 
 def create_fpn(bottom_up, detector_channels, stage_anchors, is_sigmoid_offsets=False, stage_strides=None,
                top_down_out_channels=64, input_size=640, with_batch_norm=True, with_top_down=True,
-               with_detection_module=True):
+               with_detection_module=True, num_class_outputs=1, simple_heads=True):
   inputs = Input(shape=(input_size, input_size, 3))
   normalized = L.normalize(inputs)
 
@@ -32,29 +67,4 @@ def create_fpn(bottom_up, detector_channels, stage_anchors, is_sigmoid_offsets=F
       L.ssh_detection_module(out, stage_out_channels[idx], 'det_' + str(idx), with_batch_norm=with_batch_norm) for
       idx, out in enumerate(stage_outputs)]
 
-  outputs = []
-  for stage_idx, stage_output in enumerate(stage_outputs):
-    num_anchors = len(stage_anchors[stage_idx])
-    for anchor_idx in range(0, num_anchors):
-      # backward compatibility
-      if num_anchors == 1:
-        anchor_id = 'stage_' + str(stage_idx)
-      else:
-        anchor_id = 's_' + str(stage_idx) + '_a_' + str(anchor_idx)
-
-      print(anchor_id)
-      offsets = L.conv1x1(stage_output, 2,
-                          'conv_offsets_' + anchor_id if is_sigmoid_offsets else 'output_' + anchor_id + '_offsets')
-      if is_sigmoid_offsets:
-        offsets = L.sigmoid(offsets, 'output_' + anchor_id + '_offsets')
-      scales = L.conv1x1(stage_output, 2, 'output_' + anchor_id + '_scales')
-      scores_logits = L.conv1x1(stage_output, 1, 'conv_scores_' + anchor_id)
-      scores = L.sigmoid(scores_logits, 'output_' + anchor_id + '_object')
-      scores2 = L.sigmoid(scores_logits, 'output_' + anchor_id + '_no_object')
-
-      outputs.append(offsets)
-      outputs.append(scales)
-      outputs.append(scores)
-      outputs.append(scores2)
-
-  return inputs, outputs
+  return inputs, make_heads(stage_outputs, stage_anchors, simple_heads=simple_heads, num_class_outputs=num_class_outputs, is_sigmoid_offsets=is_sigmoid_offsets)
--- colabsnippets/face_detection/fpn/keras/layers.py
+++ colabsnippets/face_detection/fpn/keras/layers.py
@@ -1,5 +1,5 @@
-from keras.layers import Lambda, UpSampling2D, Conv2D, SeparableConv2D, MaxPooling2D, \
-  BatchNormalization, Activation, Add, Concatenate
+from keras.layers import Lambda, Conv2D, SeparableConv2D, MaxPooling2D, \
+  BatchNormalization, Activation, Add, Concatenate, DepthwiseConv2D, UpSampling2D
 
 
 def normalize(x, channel_means=123):
@@ -22,13 +22,70 @@ def max_pooling(x, name):
   return MaxPooling2D(pool_size=(3, 3), strides=(2, 2), padding='same', name=name)(x)
 
 
-def conv(ConvLayer, x, channels_out, name, strides, filter_size, with_batch_norm=False):
-  out = x
-  out = ConvLayer(channels_out, filter_size, strides=strides, padding='same', use_bias=not with_batch_norm, name=name)(
-    out)
-  if with_batch_norm:
-    out = BatchNormalization(name=name + "/batch_norm")(out)
-  return out
+class Convolution:
+  def __init__(self, ConvType, channels_out, name, strides, filter_size, with_batch_norm=False):
+    self.conv = ConvType(channels_out, filter_size, strides=strides, padding='same', use_bias=not with_batch_norm,
+                         name=name)
+    self.bn_or_identity = BatchNormalization(name=name + "/batch_norm") if with_batch_norm else lambda x: x
+
+  def __call__(self, x):
+    return self.bn_or_identity(self.conv(x))
+
+
+class DepthwiseSeparableConvolution:
+  def __init__(self, channels_out, name, strides=(1, 1), filter_size=(3, 3), with_batch_norm=False):
+    self.conv = Convolution(SeparableConv2D, channels_out, name, strides, filter_size, with_batch_norm=with_batch_norm)
+
+  def __call__(self, x):
+    return self.conv(x)
+
+
+class Conv1x1:
+  def __init__(self, channels_out, name, strides=(1, 1), with_batch_norm=False):
+    self.conv = Convolution(Conv2D, channels_out, name, strides, (1, 1), with_batch_norm=with_batch_norm)
+
+  def __call__(self, x):
+    return self.conv(x)
+
+
+class XceptionReductionModule:
+  def __init__(self, channels_out, name, activate_inputs=True, with_batch_norm=False):
+    self.relu_in_or_identity = Activation('relu', name=name + "/relu_input") if activate_inputs else lambda x: x
+    self.dws_conv0 = DepthwiseSeparableConvolution(channels_out, name + '/separable_conv0',
+                                                   with_batch_norm=with_batch_norm)
+    self.relu_intermediate = Activation('relu', name=name + "/relu_intermediate")
+    self.dws_conv1 = DepthwiseSeparableConvolution(channels_out, name + '/separable_conv1',
+                                                   with_batch_norm=with_batch_norm)
+    self.max_pool = MaxPooling2D(pool_size=(3, 3), strides=(2, 2), padding='same', name=name + "/max_pooling")
+    self.conv_down = Conv1x1(channels_out, name + '/expansion_conv', strides=(2, 2), with_batch_norm=with_batch_norm)
+    self.skip = Add(name=name + '/skip_connection')
+
+  def __call__(self, x):
+    return self.skip([
+      self.max_pool(self.dws_conv1(self.relu_intermediate(self.dws_conv0(self.relu_in_or_identity(x))))),
+      self.conv_down(x)
+    ])
+
+
+class XceptionMainModule:
+  def __init__(self, channels, name, num_convs=3, with_batch_norm=False):
+    self.relus = [Activation('relu', name=name + "/relu" + str(i)) for i in range(0, num_convs)]
+    self.convs = [
+      DepthwiseSeparableConvolution(channels, name + '/separable_conv' + str(i), with_batch_norm=with_batch_norm) for i
+      in range(0, num_convs)]
+    self.skip = Add(name=name + '/skip_connection')
+
+  def __call__(self, x, is_channel_expand=False):
+    skip0 = out = x
+    for i, (relu, conv) in enumerate(zip(self.relus, self.convs)):
+      out = conv(relu(out))
+      if is_channel_expand and i == 0:
+        skip0 = out
+    return self.skip([out, skip0])
+
+
+def conv(ConvType, x, channels_out, name, strides, filter_size, with_batch_norm=False):
+  return Convolution(ConvType, channels_out, name, strides, filter_size, with_batch_norm=with_batch_norm)(x)
 
 
 def conv1x1(x, channels_out, name, with_batch_norm=False):
@@ -47,42 +104,53 @@ def conv2d_down(x, channels_out, name, with_batch_norm=False):
   return conv(Conv2D, x, channels_out, name, (2, 2), (3, 3), with_batch_norm=with_batch_norm)
 
 
-def depthwise_separable_conv2d(x, channels_out, name, with_batch_norm=False):
-  return conv(SeparableConv2D, x, channels_out, name, (1, 1), (3, 3), with_batch_norm=with_batch_norm)
-
-
-def add(name, x, y):
-  return Add(name=name)([x, y])
-
-
 def depthwise_separable_conv2d(x, channels_out, name, strides=(1, 1), with_batch_norm=False):
-  return conv(SeparableConv2D, x, channels_out, name, strides, (3, 3), with_batch_norm=with_batch_norm)
+  return DepthwiseSeparableConvolution(channels_out, name, strides, filter_size=(3, 3),
+                                       with_batch_norm=with_batch_norm)(x)
 
 
 def depthwise_separable_conv2d_down(x, channels_out, name, with_batch_norm=False):
   return depthwise_separable_conv2d(x, channels_out, name, strides=(2, 2), with_batch_norm=with_batch_norm)
 
 
-def xception_reduction_module(x, channels_out, name, activate_inputs=True, with_batch_norm=False):
-  out = x
-  out = relu(out, name + "/relu_input") if activate_inputs else out
-  out = depthwise_separable_conv2d(out, channels_out, name + '/separable_conv0', with_batch_norm=with_batch_norm)
+def depthwise_conv2d(x, name, strides=(1, 1), with_batch_norm=False):
+  out = DepthwiseConv2D((3, 3), strides=strides, padding='same', use_bias=not with_batch_norm,
+                        name=name + "/depthwise_conv")(x)
+  if with_batch_norm:
+    out = BatchNormalization(name=name + "/batch_norm")(out)
+  return out
+
+
+def depthwise_separable_conv2d_with_intermediate(x, channels_out, name, strides=(1, 1), with_batch_norm=False):
+  out = depthwise_conv2d(x, name + "/depthwise_conv", strides=strides, with_batch_norm=False)
   out = relu(out, name + '/relu_intermediate')
-  out = depthwise_separable_conv2d(out, channels_out, name + '/separable_conv1', with_batch_norm=with_batch_norm)
-  out = max_pooling(out, name + "/max_pooling")
-  expand = conv1x1_down(x, channels_out, name + '/expansion_conv', with_batch_norm=with_batch_norm)
-  return Add(name=name + '/skip_connection')([out, expand])
+  out = conv1x1(out, channels_out, name=name + "/pointwise_conv", with_batch_norm=with_batch_norm)
+  return out
+
+
+def depthwise_separable_conv2d_down_with_intermediate(x, channels_out, name, with_batch_norm=False):
+  return depthwise_separable_conv2d_with_intermediate(x, channels_out, name, strides=(2, 2), with_batch_norm=False)
+
+
+def xception_reduction_module(x, channels_out, name, activate_inputs=True, with_batch_norm=False):
+  return XceptionReductionModule(channels_out, name, activate_inputs=activate_inputs, with_batch_norm=with_batch_norm)(
+    x)
 
 
 def xception_main_module(x, channels, name, num_convs=3, with_batch_norm=False, is_channel_expand=False):
-  skip0 = x
-  out = x
-  for i in range(0, num_convs):
-    out = depthwise_separable_conv2d(relu(out, name + '/relu' + str(i)), channels, name + '/separable_conv' + str(i),
-                                     with_batch_norm=with_batch_norm)
-    if is_channel_expand and i == 0:
-      skip0 = out
-  return Add(name=name + '/skip_connection')([out, skip0])
+  return XceptionMainModule(channels, name, num_convs=num_convs, with_batch_norm=with_batch_norm)(x,
+                                                                                                  is_channel_expand=is_channel_expand)
+
+
+def mnetv2_conv(x, channels_in, exp_factor, name, is_conv_down=False, channels_out=None, with_batch_norm=False):
+  channels_out = channels_out if channels_out is not None else channels_in
+  out = relu(x, name + "/relu_input")
+  out = conv1x1(out, int(channels_in * exp_factor), name + '/expansion_conv', with_batch_norm=with_batch_norm)
+  out = relu(out, name + "/relu_expansion_conv")
+  out = depthwise_separable_conv2d(out, channels_out, name + '/separable_conv',
+                                   strides=(2, 2) if is_conv_down else (1, 1), with_batch_norm=with_batch_norm)
+  out = out if is_conv_down else Add(name=name + '/skip_connection')([x, out])
+  return out
 
 
 def ssh_context_module(x, channels, name, with_batch_norm=False):
@@ -95,6 +163,8 @@ def ssh_context_module(x, channels, name, with_batch_norm=False):
 
 
 def ssh_detection_module(x, out_channels, name, with_batch_norm=False):
+  if out_channels == 0:
+    return x
   ctx_out_1, ctx_out_2 = ssh_context_module(x, int(out_channels / 4), name + '/ctx', with_batch_norm=with_batch_norm)
   shrink = conv2d(x, int(out_channels / 2), name + '/conv_shrink', with_batch_norm=with_batch_norm)
   out = channel_concat([shrink, ctx_out_1, ctx_out_2], name + '/channel_concat')
